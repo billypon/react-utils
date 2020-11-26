c77ab7e0880ae68af2b3184c65afb05e59589828
billypon <billypon@gmail.com>
2020-11-26 10:23:24 +0800

    add checkCode to ajax

--- src/ajax.ts
+++ src/ajax.ts
@@ -1,7 +1,17 @@
 import { AxiosObservable } from 'axios-observable/lib/axios-observable.interface'
-import { Observable } from 'rxjs'
-import { map } from 'rxjs/operators'
+import { Observable, MonoTypeOperatorFunction } from 'rxjs'
+import { tap, map } from 'rxjs/operators'
 
 export function parseResponse<T = unknown>(observable: AxiosObservable<T>): Observable<T> {
   return observable.pipe(map(({ data }) => data))
+}
+
+export function checkCode(checker: (result: any) => boolean, callback: (result: any) => void): MonoTypeOperatorFunction<any> {
+  return (observable: Observable<unknown>) => {
+    return observable.pipe(tap(null, result => {
+      if (result && checker(result)) {
+        callback(result)
+      }
+    }))
+  }
 }
